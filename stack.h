fe901d44c5bfe51c0938b40f25285845656959d1
Logan Yamamoto <lkyamamo@usc.edu>
2022-10-18 03:13:17 +0000

    heap, stack, and logicism files

---
+++ stack.h
@@ -0,0 +1,23 @@
+#ifndef STACK_H
+#define STACK_H
+
+#include <vector>
+#include <stdexcept>
+
+// Use inheritance from std::vector (choose public/private) as appropriate
+template <typename T>
+class Stack 
+{
+public:
+    Stack();
+    ~Stack();
+    bool empty() const;
+    size_t size() const;
+    void push(const T& item);
+    void pop();  // throws std::underflow_error if empty
+    const T& top() const; // throws std::underflow_error if empty
+    // Add other members only if necessary
+};
+
+
+#endif
