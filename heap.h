65d3234bec8fa3c6d4e89020ff60992518829153
Logan Yamamoto <lkyamamo@usc.edu>
2022-10-19 19:46:38 -0700

    working on heap implementation

--- heap.h
+++ heap.h
@@ -2,6 +2,7 @@
 #define HEAP_H
 #include <functional>
 #include <stdexcept>
+#include <vector>
 
 template <typename T, typename PComparator = std::less<T> >
 class Heap
@@ -61,7 +62,9 @@ public:
 
 private:
   /// Add whatever helper functions and data members you need below
-
+  std::vector<T> list; //0 based heap
+  size_t m_;
+  PComparator c_;
 
 
 
@@ -81,12 +84,11 @@ T const & Heap<T,PComparator>::top() const
     // ================================
     // throw the appropriate exception
     // ================================
-
-
+    throw std::underflow_error("underflow_error");
   }
   // If we get here we know the heap has at least 1 item
   // Add code to return the top element
-
+  return list[0];
 
 
 }
@@ -101,15 +103,113 @@ void Heap<T,PComparator>::pop()
     // ================================
     // throw the appropriate exception
     // ================================
-
+    throw std::underflow_error("underflow_error");
 
   }
 
+  //set first to last value
+  list[0] = list[list.size() - 1];
+  list.pop_back();
+  //trickle down
+  size_t i = list.size() - 1;
+  while(i < list.size()){
+    //if no child stop
+    if((m_*i) + 1 >= list.size())
+    {
+      break;
+    }
+    size_t child;
+    //find best child
 
-
+    //make first child the best initially
+    child = (m_*i) + 1;
+    
+    size_t j = 2;
+    //iterate over all children to find best child
+    while((m_*i) + j < list.size() && j <= m_)
+    {
+      //if next child better than best child
+      if(c_(list[(m_*i) + j], list[child]))
+      {
+        //make it new best child
+        child = (m_*i) + j;
+      }
+      j++;
+    }
+    
+    //if child "better" than parent
+    if(c_(list[child], list[i]))
+    {
+      //swap
+      T temp = list[i];
+      list[i] = list[(i-1)/m_];
+      list[(i-1)/m_] = temp;
+    }else{
+      break;
+    }
+    
+    //go to swapped child
+    i = child;
+  }
 }
 
+template<typename T, typename PComparator>
+Heap<T, PComparator>::Heap(int m, PComparator c)
+:m_(m)
+,c_(c)
+{
+}
 
+template<typename T, typename PComparator>
+void Heap<T, PComparator>::push(const T& item)
+{
+  //add to the back
+  list.push_back(item);
 
+  //bubble up
+  size_t i = list.size() - 1;
+  while(i != 0){
+    //if left "better" than right
+    if(c_(list[i], list[(i-1)/m_]))
+    {
+      //swap
+      T temp = list[i];
+      list[i] = list[(i-1)/m_];
+      list[(i-1)/m_] = temp;
+    }else{
+      break;
+    }
+    //go to parent
+    i = (i-1)/m_;
+  }
+}
+
+template <typename T, typename PComparator>
+size_t Heap<T,PComparator>::size() const{
+  return list.size();
+}
+
+template <typename T, typename PComparator>
+bool Heap<T,PComparator>::empty() const{
+  if(Heap<T,PComparator>::size() == 0)
+  {
+    return true;
+  }
+  else{
+    return false;
+  }
+}
+
+template<typename T, typename PComparator>
+Heap<T,PComparator>::~Heap(){
+  /*
+  for(size_t i = 0; i < list.size(); i++)
+  {
+    delete list[i];
+  }
+  list.clear();
+  */
+  list.clear();
+}
 #endif
 
--- stack.h
+++ stack.h
@@ -17,7 +17,48 @@ public:
     void pop();  // throws std::underflow_error if empty
     const T& top() const; // throws std::underflow_error if empty
     // Add other members only if necessary
+private:
+    std::vector<T> list;
 };
 
+template <typename T>
+bool Stack<T>::empty() const{
+    if(list.size() == 0)
+    {
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+template <typename T>
+size_t Stack<T>::size() const{
+    return this -> list.size();
+}
+
+template <typename T>
+void Stack<T>::push(const T& item){
+    list.push_back(item);
+}
+
+template <typename T>
+void Stack<T>::pop(){
+    if(list.size() == 0){
+        throw std::underflow_error("underflow_error");
+    }else{
+        list.pop_back();
+    }
+    
+}
+
+template <typename T>
+const T& Stack<T>::top() const{
+    if(list.size() == 0){
+        throw std::underflow_error("underflow_error");
+    }else{
+        return list.back();
+    }
+}
 
 #endif
